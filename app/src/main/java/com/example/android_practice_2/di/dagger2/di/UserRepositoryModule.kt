8b4a733c25bf25c8089014d5e5000770e42f7565
Rahul9931 <rahulsaini@gmail.com>
2025-07-12 22:42:37 +0530

    using bind

--- app/src/main/java/com/example/android_practice_2/di/dagger2/di/UserRepositoryModule.kt
+++ app/src/main/java/com/example/android_practice_2/di/dagger2/di/UserRepositoryModule.kt
@@ -1,15 +1,19 @@
 package com.example.android_practice_2.di.dagger2.di
 
 import com.example.android_practice_2.di.dagger2.repository.FirebaseRepository
+import com.example.android_practice_2.di.dagger2.repository.SqlRepository
 import com.example.android_practice_2.di.dagger2.repository.UserRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryModule {
+abstract class UserRepositoryModule {
 
-    @Provides
-    fun getFirebaseRepository():UserRepository{
-        return FirebaseRepository()
-    }
+//    @Provides
+//    fun getFirebaseRepository():UserRepository{
+//        return FirebaseRepository()
+//    }
+    @Binds
+    abstract fun getSqlRepository(sqlRepository: SqlRepository) : UserRepository
 }
